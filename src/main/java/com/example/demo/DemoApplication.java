a700af5d3309ac1dcd0b9d4500d720ca3d1ebec3
Ali Dandach <alidandach1995@gmail.com>
2020-11-08 00:31:04 +0000

    AMM-01-ARI

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,19 +1,11 @@
 package com.example.demo;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
-
-	@GetMapping("/")
-	String home() {
-		return "Spring is here!";
-	}
-
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DemoApplication.class);
 	}
 }
